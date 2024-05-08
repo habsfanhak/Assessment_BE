577ae4b009609273f441412fadf71da681d1bf67
habsfanhak <56047585+habsfanhak@users.noreply.github.com>
2024-05-08 02:51:29 -0400

    commiting completed backend with delete, patch, and get routes

--- server.js
+++ server.js
@@ -7,6 +7,55 @@ const userOps = require("./userOps.js")
 
 const HTTP_PORT = process.env.PORT || 8080;
 
+app.use(express.json());
+app.use(cors())
+
+app.post("/tasks", async (req, res) => {
+    try {
+        const taskId = await userOps.returnTaskId()
+        const msg = await userOps.createTask(taskId, req.body)
+
+        res.json({ message: "Successfully processed task." });
+    }
+    catch (err) {
+        console.error("Error processing task:", err);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+})
+
+app.get("/tasks", (req, res) => {
+    userOps.getTasks()
+    .then(tasks => {
+        res.json(tasks)
+    })
+    .catch(err => {
+        console.log("Error getting tasks:", err)
+        res.status(500).json({ error: "Internal Server Error" });
+    })
+})
+
+app.patch("/tasks", (req, res) => {
+    userOps.editTask(req.body)
+    .then(function(){
+        res.json({message : "Successfully updated task."})
+    })
+    .catch(err => {
+        console.log("Error updated task:", err)
+        res.status(500).json({ error: "Internal Server Error" });
+    })
+})
+
+app.delete("/tasks", (req, res) => {
+    userOps.deleteTask(req.body)
+    .then(function(){
+        res.json({message : "Successfully deleted task."})
+    })
+    .catch(err => {
+        console.log("Error updated task:", err)
+        res.status(500).json({ error: "Internal Server Error" });
+    })
+})
+
 userOps.connect()
 .then(() => {
     app.listen(HTTP_PORT, () => { console.log("API listening on: " + HTTP_PORT) });
--- userOps.js
+++ userOps.js
@@ -10,7 +10,8 @@ let taskSchema = new Schema({
     taskName: String,
     taskDescription: String,
     taskStatus: String,
-    taskPriority: String
+    taskPriority: String,
+    dateAdded: Date
     }, { collection: 'tasks' })
 
 let Task;
@@ -30,4 +31,91 @@ module.exports.connect = function () {
         });
     });
 };
+
+module.exports.returnTaskId = function () {
+    return new Promise(function (resolve, reject) {
+        Task.find({})
+        .sort({ "taskId": -1 })
+        .limit(1)
+        .exec()
+        .then(data => {
+          if (data && data.length > 0) {
+            const lastRequestId = data[0]["taskId"];
+            resolve(lastRequestId + 1);
+          } else {
+            resolve(1);
+          }
+        })
+        .catch(err => {
+          reject(`Unable to get the largest task id: ${err}`);
+        });
+    });
+};
+
+module.exports.createTask = function (taskId, data) {
+    return new Promise(function(resolve, reject) {
+        Task.create(
+            { 'taskId': parseInt(taskId),
+                "taskName": data.taskName,
+                "taskDescription": data.taskDescription,
+                "taskStatus": data.taskStatus,
+                "taskPriority": data.taskPriority,
+                "dateAdded": data.dateAdded
+            }
+          )
+            .then((data) => {
+              resolve(data);
+            })
+            .catch((err) => {
+              reject(`Unable to create task. Error: ${err}`);
+            });
+    })
+}
+
+module.exports.getTasks = function () {
+    return new Promise(function (resolve, reject) {
+        Task.find({})
+            .exec()
+            .then(data => {
+                resolve(data)
+            }).catch(err => {
+                reject(`Unable to get tasks. Error: ${err}`);
+            });
+    });
+}
+
+module.exports.editTask = function (data) {
+    return new Promise(function (resolve, reject) {
+      Task.updateOne(
+        { '_id': data._id },
+        {
+            "taskName": data.taskName,
+            "taskDescription": data.taskDescription,
+            "taskStatus": data.taskStatus,
+            "taskPriority": data.taskPriority,
+        }
+      )
+        .exec()
+        .then((data) => {
+          resolve(data);
+        })
+        .catch((err) => {
+          reject(`Unable to update task. Error: ${err}`);
+        });
+    });
+  };
+
+  module.exports.deleteTask = function(data) {
+    return new Promise(function (resolve, reject) {
+        Task.deleteOne({ '_id': data._id }
+        ).exec()
+        .then((data) => {
+            resolve(data)
+        })
+        .catch(err => {
+            reject(`Unable to delete task. Error: ${err}`);
+        })
+    })
+}
+
 
